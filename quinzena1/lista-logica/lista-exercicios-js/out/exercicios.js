a8eeb65db48fc3c48a8bec3b92211249cf38e085
Joana Dias <dias.jo1992@gmail.com>
2021-07-22 20:00:41 -0300

    lista exercicio incompleto

---
+++ quinzena1/lista-logica/lista-exercicios-js/out/exercicios.js
@@ -0,0 +1,102 @@
+// Exemplos
+
+// Exercício 0A
+function soma() {
+  // escreva seu código aqui
+  const num1 = prompt('Digite o primeiro número')
+  const num2 = prompt('Digite o segundo número')
+
+  console.log(Number(num1) + Number(num2))
+}
+
+// Exercício 0B
+function imprimeMensagem() {
+  // escreva seu código aqui
+  const mensagem = prompt('Digite sua mensagem')
+
+  console.log(mensagem)
+}
+
+// ---------------------------------------------------
+// Exercícios
+
+// Exercício 1
+function calculaAreaRetangulo() {
+  // escreva seu código aqui
+  const altura = prompt("Digite a altura do retangulo!")
+  const largura = prompt("Digite a largura do retangulo!")
+
+  console.log(altura * largura)
+
+}
+
+// Exercício 2
+function imprimeIdade() {
+  // escreva seu código aqui
+  const anoAtual = prompt("Digite o ano atual!")
+  const anoNascimento = prompt("Digite o ano de que você nasceu")
+
+  console.log(anoAtual - anoNascimento)
+
+}
+
+// Exercício 3
+function calculaIMC() {
+  // escreva seu código aqui
+  const peso = prompt("Digite seu peso em kg!")
+  const altura = prompt("Digite sua altura em metros!")
+
+  console.log(peso / (altura * altura))
+
+}
+
+// Exercício 4
+function imprimeInformacoesUsuario() {
+  // escreva seu código aqui
+  const nome = prompt("Digite seu nome!")
+  const idade = prompt("Digite sua idade!")
+  const email = prompt("Digite seu email!")
+
+  console.log("Meu nome é " + nome + ", " + "tenho " + idade + " anos, e o meu email é " + email + ".")
+
+}
+
+// Exercício 5
+function imprimeTresCoresFavoritas() {
+  // escreva seu código aqui
+}
+
+// Exercício 6
+function retornaStringEmMaiuscula() {
+  // escreva seu código aqui
+}
+
+// Exercício 7
+function calculaIngressosEspetaculo() {
+  // escreva seu código aqui
+}
+
+// Exercício 8
+function checaStringsMesmoTamanho() {
+  // escreva seu código aqui
+}
+
+// Exercício 9
+function checaIgualdadeDesconsiderandoCase() {
+  // escreva seu código aqui
+}
+
+// Exercício 10
+function checaRenovacaoRG() {
+  // escreva seu código aqui
+}
+
+// Exercício 11
+function checaAnoBissexto() {
+  // escreva seu código aqui
+}
+
+// Exercício 12
+function checaValidadeInscricaoLabenu() {
+  // escreva seu código aqui
+}
