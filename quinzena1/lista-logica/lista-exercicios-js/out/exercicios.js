caa3369d7b1a6c1a2bbd60734162fb6f89b0e05e
Joana Dias <dias.jo1992@gmail.com>
2021-07-23 23:55:38 -0300

    projeto quinzenal1

--- quinzena1/lista-logica/lista-exercicios-js/out/exercicios.js
+++ quinzena1/lista-logica/lista-exercicios-js/out/exercicios.js
@@ -64,39 +64,97 @@ function imprimeInformacoesUsuario() {
 // Exercício 5
 function imprimeTresCoresFavoritas() {
   // escreva seu código aqui
+  let coresFavoritas = []
+  coresFavoritas[0] = prompt("Digite sua primeira cor favorita!")
+  coresFavoritas[1] = prompt("Digite sua segunda cor favorita!")
+  coresFavoritas[2] = prompt("Digite sua terceira cor favorita!")
+
+  console.log(coresFavoritas)
+
 }
 
 // Exercício 6
 function retornaStringEmMaiuscula() {
   // escreva seu código aqui
+  const palavra = prompt("Digite uma palavra com letra minuscula!")
+
+  console.log(palavra.toUpperCase())
 }
 
 // Exercício 7
 function calculaIngressosEspetaculo() {
   // escreva seu código aqui
+  const custoTotal = prompt("Digite o valor do custo total do espetáculo!")
+  const valorIngresso = prompt("Digite o valor de cada ingresso!")
+
+  console.log(custoTotal / valorIngresso)
+
 }
 
 // Exercício 8
 function checaStringsMesmoTamanho() {
   // escreva seu código aqui
+  const string1 = prompt("Digite uma palavra!")
+  const string2 = prompt("Digite outra palavra!")
+
+  console.log(string1.length === string2.length)
 }
 
 // Exercício 9
 function checaIgualdadeDesconsiderandoCase() {
   // escreva seu código aqui
+  const digitePalavra1 = prompt("Digite uma palavra!")
+  const digitePalavra2  = prompt("Digite outra palavra!")
+
+  console.log(digitePalavra1.toLowerCase() === digitePalavra2.toLowerCase())
+
+
 }
 
 // Exercício 10
 function checaRenovacaoRG() {
-  // escreva seu código aqui
+  // escreva seu código aqui 
+  let anoQueEsta = Number(prompt("Digite o ano em que estamos!"))
+  let anoQueNasceu = Number(prompt("Digite o ano do seu nasciment!"))
+  let anoRg = Number(prompt("Digite o ano em tirou sua identidade!"))
+
+  let suaIdade = anoQueEsta - anoQueNasceu
+  let emissaoRg = anoQueEsta - anoRg
+
+  let renovacao20 = suaIdade <= 20 && emissaoRg / 5 >= 1
+  let renovacao20e50 = suaIdade > 20 && suaIdade <= 50 && emissaoRg / 10 >= 1
+  let renovacao50mais = suaIdade > 50 && emissaoRg / 15 >= 1
+
+  let renovacao = renovacao20 || renovacao20e50 || renovacao50mais
+
+  console.log(renovacao)
+
 }
 
 // Exercício 11
 function checaAnoBissexto() {
   // escreva seu código aqui
+
+  let umAnoQualquer = Number(prompt("Digite um ano qualquer!"))
+
+  let multiplo400 = umAnoQualquer % 400 === 0
+  let multiplo100 = umAnoQualquer % 100 !== 0
+  let multiplo4e100 = umAnoQualquer % 4 === 0 && multiplo100
+
+  let anoBissexto = multiplo400 || multiplo4e100 
+
+  console.log(anoBissexto)
+
 }
 
 // Exercício 12
 function checaValidadeInscricaoLabenu() {
   // escreva seu código aqui
+  let condicaoEstudante1 = prompt("Condição para ser estudante Labenu: Você tem mais de 18 anos?")
+  let condicaoEstudante2 = prompt("Você possui ensino médio completo?")
+  let condicaoEstudante3 = prompt("Você possui disponibilidade exclusiva durante os horários do curso?")
+
+  let inscricaoValida = condicaoEstudante1 === "sim" && condicaoEstudante2 === "sim" && condicaoEstudante3 === "sim" 
+
+  console.log(inscricaoValida)
 }
