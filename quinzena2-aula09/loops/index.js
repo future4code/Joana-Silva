ae7a9e95d0222157fcf32d61b1684c210e8905c8
Joana Dias <dias.jo1992@gmail.com>
2021-08-05 19:38:28 -0300

    exercicio completo

--- quinzena2-aula09/loops/index.js
+++ quinzena2-aula09/loops/index.js
@@ -2,46 +2,54 @@
 //Está representando um evento que se repete, enquanto "valor" for < que 5
 // ele vai incrementar um número e o código vai rodar até chegar ao número 5.
 
-//Qual o resultado impresso no console?
-//0,1,2,3,4
+//Qual o resultado impresso no console? 
 
-// let valor = 0
-// for(let i = 0; i < 5; i++) {
-//   valor += i
-// }
-// console.log(valor)
+
+let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor)
 
 //2. Leia o código abaixo:
-// const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
-// for (let numero of lista) {
-//   if (numero > 18) {
-// 		console.log(numero)
-// 	}
-// }
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+}
 //a) O que vai ser impresso no console?
 // R: "19, 21, 23, 25, 27 30"
 
 //b) Se eu quisesse acessar o **índice** de cada elemento dessa lista, o `for...of...`
 // é suficiente? Se sim, o que poderia ser usado para fazer isso?
-//R: É sim o suficiente, seria só eu tirar o if e dar console.log em numero
-// Exemplo:const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
-//          for (let numero of lista) {
-//          console.log(numero)
-//         }
+//R: É sim o suficiente, seria só eu criar a variavel i = 0 e dar console.log na var i 
+// e incrementar i++.
+/*Exemplo: const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+           let i = 0 
+           for (let numero of lista) {
+           console.log(`indice ${i} é o número: ${lista[i]}`); i++;
+           } */
 
 //3. Qual seria o resultado impresso no console, se o usuário digitasse o número `4` ?
 
-    // const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
-    // let quantidadeAtual = 0
-    // while(quantidadeAtual < quantidadeTotal){
-    //   let linha = ""
-    //   for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
-    //     linha += "*"
-    //   }
-    //   console.log(linha)
-    //   quantidadeAtual++
-    // }
-    // ?????????????????????????????????????????????????????????
+    const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+    let quantidadeAtual = 0
+    while(quantidadeAtual < quantidadeTotal){
+      let linha = ""
+      for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+        linha += "*"
+      }
+      console.log(linha)
+      quantidadeAtual++
+    }
+
+    /* Resposta: *
+                 **
+                 ***
+                 ****  
+    */
+    
 
     //Exercícios de escrita de código**
      //1. Pergunte ao usuário quantos bichinhos de estimação ele tem e guarde esse dado em uma variável. 
@@ -53,79 +61,82 @@
         //c) Por fim, imprima o array com os nomes dos bichinhos no console
 
         let petUsuario = Number(prompt('Quantos animais de estimação você tem?'))
-        let quantidadePetUsurario = 0
+        if (petUsuario === 0) {
+            console.log("Que pena! Você pode adotar um pet!")
+        } for (let i = 0; i < petUsuario; i++) {
+           let arrayNomePet = []
+           let nomePetUsuario = prompt("Que legal! Digite o nome de um do seus pet!")
+           arrayNomePet = nomePetUsuario
+        
+           console.log(nomePetUsuario)
+        }
+        
+        
 
-        for (petUsuario <= 0) {
-        console.log("Que pena! Você pode adotar um pet!")
-        }  
+    // 2. Considere que você tenha acesso a um `array`  (chamado de 'array original') que é composto
+    // somente de números. Baseando-se nisso, crie uma função para cada um dos itens abaixo, 
+    // realizando as operações pedidas:
 
-    // 2. Considere que você tenha acesso a um `array`  (chamado de 'array original') que é composto somente de números. Baseando-se nisso, crie uma função para cada um dos itens abaixo, realizando as operações pedidas:
+      const arrayOriginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
 
     //     a) Escreva um programa que **imprime** cada um dos valores do array original.
 
-    //     b) Escreva um programa que **imprime** cada um dos valores do array original divididos por 10
+               for (let arrayDeNumeros of arrayOriginal) {
+                   console.log(arrayDeNumeros)
+               }
 
-    //     c) Escreva um programa que **crie** um novo array contendo, somente, os números pares do array original e **imprima** esse novo array
+    // //     b) Escreva um programa que **imprime** cada um dos valores do array original divididos por 10
+             
+                for (let arrayDeNumeros of arrayOriginal) {
+                   console.log(arrayDeNumeros / 10)
+                 }
 
-    //     d) Escreva um programa que **crie** um novo array contendo strings, da seguinte forma: "O elemento do índex `i` é: `numero`". Depois, **imprima** este novo array.
+    // //     c) Escreva um programa que **crie** um novo array contendo, somente, os números pares do
+    // //        array original e **imprima** esse novo array
+             
+              function retornaNumeroPar(arrayOriginal) {
+                  let numeroPar = []
+                  for (let i = 0; i < arrayOriginal.length; i++){
+                      if (arrayOriginal[i] % 2 === 0) {
+                          numeroPar.push(arrayOriginal[i])
+                      }
+                  }
+                  return numeroPar
+              }
+               console.log(retornaNumeroPar(arrayOriginal))
+
+    //     d) Escreva um programa que **crie** um novo array contendo strings, da seguinte forma: 
+    //     "O elemento do índex `i` é: `numero`". Depois, **imprima** este novo array.
+
+          function novoArrayDeString() {
+              let arrayString = []
+              let i = 0
+              for (let numerosArray of arrayOriginal) {
+                  arrayString.push(`O elemento do índex ${i} é: ${numerosArray}`); i++;
+
+              }
+              return arrayString
+          }
+           console.log(novoArrayDeString())
 
     //     e) Escreva um programa que imprima no console o maior e o menor números contidos no array original
+           
+          let maiorNumero = 0
+          let menorNumero = 140
+          for (let numeros of arrayOriginal) {
+          if (numeros > maiorNumero) {
+             maiorNumero = numeros
+             } if (numeros < menorNumero) {
+                 menorNumero = numeros
+             } 
+          }
+          console.log(`Maior número é: ${maiorNumero} e o menor é:${menorNumero}`) 
+          
+        
+          
+        
+          
+          
+        
 
-    //     - 💡 Dica
-
-    //         Você deve criar variáveis para guardar o `valorMaximo` e o `valorMinimo`. Inicialize a variável `valorMaximo` com um valor que não seja maior que qualquer valor do array original e a `valorMinimo` com um valor que não seja menor que qualquer valor do array original.
-
-    //     - Exemplo de saídas esperadas
-
-    //         ```jsx
-    //         // Este array será usado para exemplificar as respostas de todos os itens
-    //         const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
-
-    //         // Resposta item a.
-    //         80
-    //         30
-    //         130
-    //         40
-    //         60
-    //         21
-    //         70
-    //         120
-    //         90
-    //         103
-    //         110
-    //         55
-
-    //         // Resposta item b.
-    //         8
-    //         3 
-    //         13
-    //         4
-    //         6
-    //         2.1
-    //         7 
-    //         12
-    //         9
-    //         10.3
-    //         11 
-    //         5.5
-
-    //         // Resposta item c.
-    //         [80, 30, 130, 40, 60, 70, 120, 90, 110] 
-
-    //         // Resposta item d.
-    //         [ 'O elemento do índex 0 é 80',
-    //           'O elemento do índex 1 é 30',
-    //           'O elemento do índex 2 é 130',
-    //           'O elemento do índex 3 é 40',
-    //           'O elemento do índex 4 é 60',
-    //           'O elemento do índex 5 é 21',
-    //           'O elemento do índex 6 é 70',
-    //           'O elemento do índex 7 é 120',
-    //           'O elemento do índex 8 é 90',
-    //           'O elemento do índex 9 é 103',
-    //           'O elemento do índex 10 é 110',
-    //           'O elemento do índex 11 é 55' ]
-
-    //         // Resposta e.
-    //         "O maior número é 130 e o menor é 21"
-    //         ```
+    
