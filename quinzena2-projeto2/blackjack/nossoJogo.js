e7d5f9d8c422f38a7e7d0975ad94fd51ee5f3d1f
Joana Dias <dias.jo1992@gmail.com>
2021-08-06 22:56:10 -0300

    feito

--- quinzena2-projeto2/blackjack/nossoJogo.js
+++ quinzena2-projeto2/blackjack/nossoJogo.js
@@ -1,14 +1,4 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- */
+
 
     console.log("Bem vindo(a) ao jogo de Blackjack!")
     
@@ -22,7 +12,7 @@
 
     let respostaUsuario = confirm("Quer Iniciar uma nova rodada?")
   
-     while (respostaUsuario = true) {
+     while (respostaUsuario === true) {
 
      if(usuario === computador) {
       console.log(`Usuário - cartas: ${cartaUsuario1.texto} ${cartaUsuario2.texto} - pontuação: ${usuario}`);
@@ -39,12 +29,12 @@
       console.log(`Computador - cartas: ${cartaComputador1.texto} ${cartaComputador2.texto} - pontuação: ${computador}`);
       console.log("O Computador Ganhou!")
     }
-    
+
     break
 
     }
 
-    while (respostaUsuario = false) {
+    while (respostaUsuario === false) {
       console.log("O jogo acabou!")
       break
    }
