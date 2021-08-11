f268891a03e2e3e22a7b2c01281092390e395b8f
Joana Dias <dias.jo1992@gmail.com>
2021-08-11 00:22:05 -0300

    Exercicio aula-10

--- quinzena2-aula10/funcoes-de-array/index.js
+++ quinzena2-aula10/funcoes-de-array/index.js
@@ -1,14 +1,14 @@
 // 1.  Leia o código abaixo
 
-// const usuarios = [
-//     { nome: "Amanda Rangel", apelido: "Mandi" },
-//     { nome: "Laís Petra", apelido: "Laura" },
-//     { nome: "Letícia Chijo", apelido: "Chijo" }
-//   ]
+const usuarios = [
+    { nome: "Amanda Rangel", apelido: "Mandi" },
+    { nome: "Laís Petra", apelido: "Laura" },
+    { nome: "Letícia Chijo", apelido: "Chijo" }
+  ]
   
-//   const novoArrayA = usuarios.map((item, index, array) => {
-//      console.log(item, index, array)
-//   })
+  const novoArrayA = usuarios.map((item, index, array) => {
+     console.log(item, index, array)
+  })
 
   //a) O que vai ser impresso no console?
   //nome: "Amanda Rangel", apelido: "Mandi" 
@@ -18,39 +18,40 @@
 
   //2. Leia o código abaixo
 
-    // const usuarios = [
-    //   { nome: "Amanda Rangel", apelido: "Mandi" },
-    //   { nome: "Laís Petra", apelido: "Laura" },
-    //   { nome: "Letícia Chijo", apelido: "Chijo" },
-    // ]
+    const usuarios = [
+      { nome: "Amanda Rangel", apelido: "Mandi" },
+      { nome: "Laís Petra", apelido: "Laura" },
+      { nome: "Letícia Chijo", apelido: "Chijo" },
+    ]
 
-    // const novoArrayB = usuarios.map((item, index, array) => {
-    //    return item.nome
-    // })
+    const novoArrayB = usuarios.map((item, index, array) => {
+       return item.nome
+    })
 
-    // console.log(novoArrayB)
+    console.log(novoArrayB)
 
     //a) O que vai ser impresso no console?
     // "Amanada Rangel" "Laís Petra" "Letícia Chijo"
 
     //3. Leia o código abaixo
 
-//   const usuarios = [
-//     { nome: "Amanda Rangel", apelido: "Mandi" },
-//     { nome: "Laís Petra", apelido: "Laura" },
-//     { nome: "Letícia Chijo", apelido: "Chijo" },
-//   ]
+  const usuarios = [
+    { nome: "Amanda Rangel", apelido: "Mandi" },
+    { nome: "Laís Petra", apelido: "Laura" },
+    { nome: "Letícia Chijo", apelido: "Chijo" },
+  ]
   
-//   const novoArrayC = usuarios.filter((item, index, array) => {
-//      return item.apelido !== "Chijo"
-//   })
+  const novoArrayC = usuarios.filter((item, index, array) => {
+     return item.apelido !== "Chijo"
+  })
   
-//   console.log(novoArrayC)
+  console.log(novoArrayC)
 
   //a) O que vai ser impresso no console?
-  //"Mandi" "Laura"
+  //nome: "Amanda Rangel", apelido: "Mandi"
+  //nome: "Laís Petra", apelido: "Laura"
 
-  //1. 1. Dado o seguinte array de cachorrinhos que são clientes de um pet shop,
+  //1. Dado o seguinte array de cachorrinhos que são clientes de um pet shop,
   // realize as operações pedidas nos itens abaixo utilizando as funções de array **map** e **filter:**
 
     const pets = [
@@ -78,5 +79,16 @@
 
     //c) Crie um novo array que possuirá mensagens para enviar para todos os clientes que são poodles.
     // A mensagem deve ser: "Você ganhou um cupom de desconto de 10% para tosar o/a `[NOME]`!"
+
+    const mensagemDePromocao = pets.filter((item, index, array) => {
+       if (item.raca === "Poodle")
+       return item.nome
+    })
+     function imprimeMensagem (pets){
+     return  `Você ganhou um cupom de desconto de 10% para tosar o/a ${pets.nome}`
+
+     }
+     const mensagem = mensagemDePromocao.map(imprimeMensagem)
+     console.log(mensagem)
 
     
