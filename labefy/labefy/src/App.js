5024fe136bb924b6aed11e78f53c1cbdaf3bc2e8
jojodias28 <dias.jo1992@gmail.com>
2021-10-16 00:28:29 -0300

    última versão

--- labefy/labefy/src/App.js
+++ labefy/labefy/src/App.js
@@ -1,7 +1,8 @@
 import React from "react"
-import styled from 'styled-components' 
 import  CriarPlaylist from "./Components/CriarPlaylist"
+import DetalhesPlaylist from "./Components/DetalhesPlaylist"
 import ListaPlaylist from "./Components/ListaPlaylist"
+
 
 class App extends React.Component {
 
